9fdb88af7c2da7ad07c550bc8a380f8c1fed1155
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-07-29 01:15:59 -0300

    Ordenação

--- src/main/java/com/projeto/forum/controller/TopicosController.java
+++ src/main/java/com/projeto/forum/controller/TopicosController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,9 +42,9 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, @RequestParam Integer pagina, @RequestParam Integer qtd) {
+	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, @RequestParam Integer pagina, @RequestParam Integer qtd, @RequestParam String ordenacao) {
 		
-		Pageable paginacao = PageRequest.of(pagina, qtd);
+		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordenacao);
 		
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
