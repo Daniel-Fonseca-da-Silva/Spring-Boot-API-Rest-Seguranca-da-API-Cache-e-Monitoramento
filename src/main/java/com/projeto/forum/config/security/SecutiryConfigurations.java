591d8992d29b9ab46ffe58f45b1a937beb8bc1b2
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-08-01 00:41:44 -0300

    Liberando acesso aos endpoints públicos

--- src/main/java/com/projeto/forum/config/security/SecutiryConfigurations.java
+++ src/main/java/com/projeto/forum/config/security/SecutiryConfigurations.java
@@ -1,11 +1,33 @@
 package com.projeto.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecutiryConfigurations extends WebSecurityConfigurerAdapter {
+	
+	// Configurações de autenticação (controle de acesso, login etc)
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	}
+	
+	// Configurações de autorização (url, perfil de acesso)
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+	}
+	
+	// Configurações de arquivos estaticos (js, css, imagens, etc)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
 	
 }
