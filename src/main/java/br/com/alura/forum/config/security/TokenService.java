fe21bf4500006e8f51c6255d6ce385c8012390e0
Daniel Fonseca da Silva <developer-web@programmer.net>
2022-08-14 21:48:58 -0300

    Autenticando o cliente via Spring Security

--- src/main/java/br/com/alura/forum/config/security/TokenService.java
+++ src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.forum.modelo.Usuario;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -41,8 +42,11 @@ public class TokenService {
 			e.printStackTrace();
 			return false;
 		}
-		
-		
+	}
+
+	public Long getIdUsuario(String token) {
+		Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+		return Long.parseLong(claims.getSubject());
 	}
 
 }
